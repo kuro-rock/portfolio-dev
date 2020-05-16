0d68705b9b6f2aa0399fc9287541b7c3cf99f4d4
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-05-16 15:30:07 +0900

    implement: components -> footer

---
+++ components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="l-footer">
+    <div class="footer-inner">
+      <small>&copy; 2020 sparking.world</small>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
