a65b70d58ff08fcf1c9ee6a1e49760960dc280fb
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-05-17 11:19:05 +0900

    modify: pages -> about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,20 +1,31 @@
 <template>
   <main class="l-main">
-    <div class="bl_jumbotron">
-      <img src="" alt="" />
-    </div>
-    <h2>About</h2>
-    <p>宮崎県出身 1982年生まれ</p>
-    <p>
-      趣味は釣り・ドライブ。特技は乗馬。
-    </p>
-    <p>
-      大学ではデザインやインスタレーションアートを専攻し、プログラミングによる視覚表現を研究。
-      社会人となってからもデザインとプログラミングという強みを活かし、WEB広告やECサイト構築などにおけるフロントエンド領域を中心として幅広く経験を積む。
-    </p>
-    <p>
-      近年はリーダー職として開発チームの方針策定やメンバー教育にも力を入れています。
-    </p>
+    <section class="bl-section">
+      <div class="bl-media">
+        <figure class="bl-media-img">
+          <img src="~/assets/images/myself.jpg" alt="著者近影" />
+        </figure>
+        <div class="bl-media-body">
+          <h2 class="el-heading2">About</h2>
+          <p>黒田 訓弘</p>
+          <p>宮崎県出身 1982年生まれ</p>
+          <p>
+            趣味は釣り・ドライブ。特技は乗馬。
+          </p>
+          <p>
+            大学ではデザインやインスタレーションアートを専攻し、プログラミングによる視覚表現を研究。
+            <br />社会人となってからもデザインとプログラミングという強みを活かし、
+            <br />WEB広告やECサイト構築などにおけるフロントエンド領域を中心として幅広く経験を積む。
+          </p>
+          <p>
+            近年はリーダー職として開発チームの方針策定やメンバー教育にも力を入れています。
+          </p>
+        </div>
+      </div>
+      <footer class="hp-txt-center">
+        <nuxt-link to="/" class="el-link-more">もどる</nuxt-link>
+      </footer>
+    </section>
   </main>
 </template>
 
